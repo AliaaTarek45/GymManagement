240233e609db86e6eeb1389b5e4244f17c051dc8
Aliaa Tarek <Aliaa Tarek@LAPTOP-LJC895S6>
2026-05-08 14:18:21 +0300

    Add project files.

---
+++ GymManagementBLL/Services/Classes/AccountService.cs
@@ -0,0 +1,24 @@
+﻿using GymManagementBLL.Services.Interfaces;
+using GymManagementBLL.ViewModels.AccountViewModels;
+using GymManagementDAL.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace GymManagementBLL.Services.Classes
+{
+	public class AccountService : IAccountService
+	{
+		private readonly UserManager<ApplicationUser> _userManager;
+
+		public AccountService(UserManager<ApplicationUser> userManager)
+		{
+			_userManager = userManager;
+		}
+		public ApplicationUser? ValidateUser(LoginViewModel LoginViewModel)
+		{
+			var User = _userManager.FindByEmailAsync(LoginViewModel.Email).Result;
+			if (User is null) return null;
+			var isValid = _userManager.CheckPasswordAsync(User, LoginViewModel.Password).Result;
+			return isValid ? User : null;
+		}
+	}
+}
---
+++ GymManagementDAL/Repositories/Classes/SessionRepository.cs
@@ -0,0 +1,40 @@
+﻿using GymManagementDAL.Data.Contexts;
+using GymManagementDAL.Entities;
+using GymManagementDAL.Repositories.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace GymManagementDAL.Repositories.Classes
+{
+	public class SessionRepository : GenericRepository<SessionEntity>, ISessionRepository
+	{
+		private readonly GymDbContext _dbContext;
+
+		public SessionRepository(GymDbContext dbContext) : base(dbContext)
+		{
+			_dbContext = dbContext;
+		}
+		public IEnumerable<SessionEntity> GetAllSessionsWithTrainerAndCategory(Func<SessionEntity, bool>? condition = null)
+		{
+			if (condition is null)
+				return _dbContext.Sessions.Include(X => X.Trainer)
+					.Include(X => X.Category)
+					.ToList();
+			else
+				return _dbContext.Sessions.Include(X => X.Trainer)
+					.Include(X => X.Category).ToList()
+					.Where(condition);
+		}
+
+		public int GetCountOfBookedSlots(int SessionId)
+		{
+			return _dbContext.Bookings.Where(X => X.SessionId == SessionId).Count();
+		}
+
+		public SessionEntity? GetSessionWithTrainerAndCategory(int SessionId)
+		{
+			return _dbContext.Sessions.Include(X => X.Trainer)
+									  .Include(X => X.Category)
+									  .FirstOrDefault(X => X.Id == SessionId);
+		}
+	}
+}
